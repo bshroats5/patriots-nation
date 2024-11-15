4d7932a41d04e5388c9745af061e3732f177ac6d
Bret Shroats <bshroats5@gmail.com>
2024-11-14 21:03:40 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+# Sample data for blog posts
+posts = [
+    {
+        'title': 'Exciting Game Highlights',
+        'date': 'January 1, 2023',
+        'author': 'Author Name',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.',
+        'slug': 'exciting-game-highlights'
+    },
+    {
+        'title': 'Player Spotlight: John Doe',
+        'date': 'January 2, 2023',
+        'author': 'Author Name',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.',
+        'slug': 'player-spotlight-john-doe'
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', posts=posts)
+
+@app.route('/post/<slug>')
+def post(slug):
+    post = next((p for p in posts if p['slug'] == slug), None)
+    return render_template('post.html', post=post)
+
+if __name__ == '__main__':
+    app.run(debug=True)
