7c2d98c0a3fe031bea150f12d89ccedfc416b979
Bret Shroats <bshroats5@gmail.com>
2025-05-13 21:52:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ posts = [
 ]
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+# @app.route('/index')
+def index(): # Renamed function to 'index'
+    return render_template('index.html', posts=posts) # Pass the posts variable
 
 @app.route('/base')
 def base():
